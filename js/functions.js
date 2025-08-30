4beebb08daafd3a0fdf59ce3256d9bfb7d73a3f2
fe-barbosah <fe.barbosahh@gmail.com>
2025-08-30 16:52:33 -0300

    Add files via upload

---
+++ js/functions.js
@@ -0,0 +1,153 @@
+﻿var $window = $(window), gardenCtx, gardenCanvas, $garden, garden;
+var clientWidth = $(window).width();
+var clientHeight = $(window).height();
+
+$(function () {
+    // setup garden
+	$loveHeart = $("#loveHeart");
+	var offsetX = $loveHeart.width() / 2;
+	var offsetY = $loveHeart.height() / 2 - 55;
+    $garden = $("#garden");
+    gardenCanvas = $garden[0];
+	gardenCanvas.width = $("#loveHeart").width();
+    gardenCanvas.height = $("#loveHeart").height()
+    gardenCtx = gardenCanvas.getContext("2d");
+    gardenCtx.globalCompositeOperation = "lighter";
+    garden = new Garden(gardenCtx, gardenCanvas);
+	
+	$("#content").css("width", $loveHeart.width() + $("#code").width());
+	$("#content").css("height", Math.max($loveHeart.height(), $("#code").height()));
+	$("#content").css("margin-top", Math.max(($window.height() - $("#content").height()) / 2, 10));
+	$("#content").css("margin-left", Math.max(($window.width() - $("#content").width()) / 2, 10));
+
+    // renderLoop
+    setInterval(function () {
+        garden.render();
+    }, Garden.options.growSpeed);
+});
+
+$(window).resize(function() {
+    var newWidth = $(window).width();
+    var newHeight = $(window).height();
+    if (newWidth != clientWidth && newHeight != clientHeight) {
+        location.replace(location);
+    }
+});
+
+function getHeartPoint(angle) {
+	var t = angle / Math.PI;
+	var x = 19.5 * (16 * Math.pow(Math.sin(t), 3));
+	var y = - 20 * (13 * Math.cos(t) - 5 * Math.cos(2 * t) - 2 * Math.cos(3 * t) - Math.cos(4 * t));
+	return new Array(offsetX + x, offsetY + y);
+}
+
+function getHibiscusColor() {
+    // Hibiscus: vermelho, rosa, amarelo, branco
+    var colors = [
+        "rgb(220,20,60)",    // vermelho hibisco
+        "rgb(255,182,193)",  // rosa claro
+        "rgb(255,255,0)",    // amarelo
+        "rgb(255,255,255)",  // branco
+        "rgb(255,105,180)",  // rosa forte
+        "rgb(255,69,0)"      // vermelho alaranjado
+    ];
+    return colors[Math.floor(Math.random() * colors.length)];
+}
+
+function startHeartAnimation() {
+	var interval = 50;
+	var angle = 10;
+	var heart = new Array();
+	var animationTimer = setInterval(function () {
+		var bloom = getHeartPoint(angle);
+		var draw = true;
+		for (var i = 0; i < heart.length; i++) {
+			var p = heart[i];
+			var distance = Math.sqrt(Math.pow(p[0] - bloom[0], 2) + Math.pow(p[1] - bloom[1], 2));
+			if (distance < Garden.options.bloomRadius.max * 1.3) {
+				draw = false;
+				break;
+			}
+		}
+		if (draw) {
+			heart.push(bloom);
+			// Passe a cor hibisco para o método de criação do bloom
+			garden.createRandomBloom(bloom[0], bloom[1], getHibiscusColor());
+		}
+		if (angle >= 30) {
+			clearInterval(animationTimer);
+			showMessages();
+		} else {
+			angle += 0.2;
+		}
+	}, interval);
+}
+
+(function($) {
+	$.fn.typewriter = function() {
+		this.each(function() {
+			var $ele = $(this), str = $ele.html(), progress = 0;
+			$ele.html('');
+			var timer = setInterval(function() {
+				var current = str.substr(progress, 1);
+				if (current == '<') {
+					progress = str.indexOf('>', progress) + 1;
+				} else {
+					progress++;
+				}
+				$ele.html(str.substring(0, progress) + (progress & 1 ? '_' : ''));
+				if (progress >= str.length) {
+					clearInterval(timer);
+				}
+			}, 75);
+		});
+		return this;
+	};
+})(jQuery);
+
+function timeElapse() {
+    // Data de início: 20/04/2025
+    var startDate = new Date(2025, 3, 20); // mês começa do zero (0 = janeiro)
+    var currentDate = new Date();
+    var seconds = Math.floor((currentDate - startDate) / 1000);
+
+    var dias = Math.floor(seconds / (3600 * 24));
+    seconds = seconds % (3600 * 24);
+    var horas = Math.floor(seconds / 3600);
+    if (horas < 10) {
+        horas = "0" + horas;
+    }
+    seconds = seconds % 3600;
+    var minutos = Math.floor(seconds / 60);
+    if (minutos < 10) {
+        minutos = "0" + minutos;
+    }
+    seconds = seconds % 60;
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
+    var resultado = "<span class=\"digit\">" + dias + "</span> dias <span class=\"digit\">" + horas + "</span> horas <span class=\"digit\">" + minutos + "</span> minutos <span class=\"digit\">" + seconds + "</span> segundos";
+    $("#elapseClock").html(resultado);
+}
+
+function showMessages() {
+	adjustWordsPosition();
+	$('#messages').fadeIn(5000, function() {
+		showLoveU();
+	});
+}
+
+function adjustWordsPosition() {
+	$('#words').css("position", "absolute");
+	$('#words').css("top", $("#garden").position().top + 195);
+	$('#words').css("left", $("#garden").position().left + 70);
+}
+
+function adjustCodePosition() {
+	$('#code').css("margin-top", ($("#garden").height() - $("#code").height()) / 2);
+}
+
+function showLoveU() {
+	$('#loveu').fadeIn(3000);
+}
